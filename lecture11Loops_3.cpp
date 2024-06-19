9eda838b31fb14b2bc506fb41559dde4fea1c9b8
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-19 17:03:53 +0530

    Add files via upload

---
+++ lecture11Loops_3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+ 
+   
+
+    int i=0;
+ // Demonsration of while loop
+    while(i < 5){
+        cout << i << " ";
+        i++;
+    }
+    cout<<endl;
+
+   
+
+
+
+
+}
